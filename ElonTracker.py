927bde235dabd58bc61843b7a9e291fde31d8981
Denzel  :) <61935369+denzelxrt@users.noreply.github.com>
2022-12-18 13:26:04 +0000

    Update ElonTracker.py

--- ElonTracker.py
+++ ElonTracker.py
@@ -5,7 +5,7 @@ from twilio.rest import Client
 api_key = "YOUR_API_KEY"
 
 # The FlightAware identifier for Elon Musk's plane
-ident = "N628TS", "N272BG", "N502SX"
+ident = "N272BG"
 
 # Your Twilio account SID and auth token
 account_sid = "YOUR_ACCOUNT_SID"
